ead83fc4c69ed04e19245b84ad9475b335eda42d
Faizan09876 <malikfaizan34@gmail.com>
2021-08-07 17:08:04 +0500

    Last Commit ( Animations done)

--- app/src/main/java/com/faizan/animationsinandroid/MainActivity.java
+++ app/src/main/java/com/faizan/animationsinandroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imglion;
     private ImageView imgleopard;
     private boolean isLoinShowing;
+    private TextView  txtAndroid;
+    private Button btnAnimate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         imgleopard = findViewById(R.id.imgleopard);
         txthello =findViewById(R.id.txthello);
         txthi = findViewById(R.id.texthi);
+        txtAndroid = findViewById(R.id.txtAndroid);
+        btnAnimate = findViewById(R.id.btnAnimate);
+
+        txthello.setX(2000);
+        txthi.setX(-2000);
+        txtAndroid.setY(-3000);
+
         txthello.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,20 +66,42 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Chocha billa", Toast.LENGTH_SHORT).show();
                 if(isLoinShowing){
 
-                    imglion.animate().alpha(0f).setDuration(4000);
-                    imgleopard.animate().alpha(1f).setDuration(4000);
+                    imglion.animate().alpha(0f).setDuration(2000);
+                    imgleopard.animate().alpha(1f).setDuration(2000);
                     isLoinShowing = false;
 
                 }
                 else if(!isLoinShowing){
-                    imgleopard.animate().alpha(0f).setDuration(4000);
-                    imglion.animate().alpha(1f).setDuration(4000);
+                    imgleopard.animate().alpha(0f).setDuration(2000);
+                    imglion.animate().alpha(1f).setDuration(2000);
                     isLoinShowing = true;
 
                 }
 
             }
         });
+
+        txtAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                txtAndroid.animate().translationX(1500).setDuration(9000);
+//                txtAndroid.animate().rotation(360).setDuration(9000);
+
+                txtAndroid.animate().rotationX(400).translationY(-2000).setDuration(8000);
+
+            }
+        });
+
+        btnAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txthello.animate().translationXBy(-2000).rotation(360).setDuration(4000);
+                txthi.animate().translationXBy(2150).alpha(1f).setDuration(4000);
+                txtAndroid.animate().translationYBy(3000).scaleX(1).setDuration(4000);
+            }
+        });
+
 
 
     }
