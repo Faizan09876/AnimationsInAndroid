157ee7760f7027003f16a3b069ed23ab9b88a22d
Faizan09876 <malikfaizan34@gmail.com>
2021-08-07 14:00:04 +0500

    First commit ( Text fade and other appear)

--- app/src/main/java/com/faizan/animationsinandroid/MainActivity.java
+++ app/src/main/java/com/faizan/animationsinandroid/MainActivity.java
@@ -3,12 +3,29 @@ package com.faizan.animationsinandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txthello;
+    private TextView txthi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txthello =findViewById(R.id.txthello);
+        txthi = findViewById(R.id.texthi);
+        txthello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   Log.i("Button Tag"," Button tapped!");
+                txthello.animate().alpha(0f).setDuration(3000);
+                txthi.animate().alpha(1f).setDuration(3000);
+            }
+        });
     }
 }
